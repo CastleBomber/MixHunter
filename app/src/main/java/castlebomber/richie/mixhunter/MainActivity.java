2e3551a9152f82bccb1f4a708a165b0808bf6e52
cBombs <richie.ucsb@gmail.com>
2020-05-04 16:20:32 -0700

    cool_menu working on items, may need subitems

--- app/src/main/java/castlebomber/richie/mixhunter/MainActivity.java
+++ app/src/main/java/castlebomber/richie/mixhunter/MainActivity.java
@@ -2,12 +2,20 @@ package castlebomber.richie.mixhunter;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
+
+
+// VIEW BINDING?
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,28 +27,52 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: started.");
 
         initImageBitmaps();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.cool_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
     private void initImageBitmaps(){
         Log.d(TAG, "initImageBitmaps: preparing bitmaps.");
 
-        mImageUrls.add("https://i.redd.it/tpsnoz5bzo501.jpg");
+        mImageUrls.add("https://i.imgur.com/clsV6zv.png");
         mNames.add("SoundCloud");
 
-        mImageUrls.add("https://i.redd.it/qn7f9oqu7o501.jpg");
-        mNames.add("Apple");
+        mImageUrls.add("https://i.imgur.com/XPlbYju.jpg");
+        mNames.add("Spotify");
 
-        mImageUrls.add("https://i.redd.it/j6myfqglup501.jpg");
+        mImageUrls.add("https://i.imgur.com/UJML2nf.png");
         mNames.add("Youtube");
 
-
-        mImageUrls.add("https://i.redd.it/0h2gm1ix6p501.jpg");
-        mNames.add("Spotify");
-        
+        mImageUrls.add("https://i.imgur.com/iJMmbVf.jpg");
+        mNames.add("AppleMusic");
 
         initRecyclerView();
     }
